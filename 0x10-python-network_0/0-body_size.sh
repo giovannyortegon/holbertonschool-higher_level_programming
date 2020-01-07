868833fe4615cf2166f1b3ed8b389bbe3664fe1c
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-07 14:17:52 -0500

    [UPDATE] File 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, 
 # and displays the size of the body of the response
-curl -Is $1 | grep -oP "(?<=Content-Length: ).*"
+curl -Is "$1" | grep Content-Length: | sed 's/Content-Length: //g'
