92260acce46a1df7dd1393c775c83b450ad3b3d9
Skillhh <ortegon.giovanny@hotmail.com>
2020-01-07 19:14:08 -0500

    [UPDATE]

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Script that makes a request that causes the server to respond with a message.
+# Script that makes a request that causes the server to respond with a message.
 curl -sLX PUT -H "origin:HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
