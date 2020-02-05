926f3a5af7fef8980e1f732e0cd3f106de44dcdc
Skillhh <ortegon.giovanny@hotmail.com>
2020-02-04 16:58:06 -0800

    [ADD] File 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Prints the first argument passed to it.
+const Args = process.argv.slice(2);
+let len = 0;
+
+Args.forEach(i => { len++; });
+
+if (len === 0) {
+  console.log('No argument');
+} else {
+  console.log(Args[len - 1]);
+}
