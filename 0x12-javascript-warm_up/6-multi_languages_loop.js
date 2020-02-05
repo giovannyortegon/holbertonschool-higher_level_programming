8b8b8fc51098c8551a1eb34a2afa6ce4f4c187e6
Skillhh <ortegon.giovanny@hotmail.com>
2020-02-04 17:53:20 -0800

    [ADD] File 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 // Prints the first argument passed to it.
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-array.forEach(idx => { console.log(idx); });
+let len = 0;
+for (; len < array.length; len++) {
+  console.log(array[len]);
+}
