1b9146adea210ac4031be40348ba4d15f624e5e2
Skillhh <ortegon.giovanny@hotmail.com>
2020-02-04 21:12:12 -0800

    [ADD] File 103

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Update this script by adding a new function
+// incr that increments the integer value.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+  return this;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
